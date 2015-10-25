44f3db46f2307edc5103e1bffbf7c7b6e2763535
Quentin <fayet.quentin@gmail.com>
2015-10-25 17:54:48 +0100

    Add copy bower component gulp

--- client/gulpfile.js
+++ client/gulpfile.js
@@ -2,14 +2,24 @@ var gulp = require('gulp');
 var ngAnnotate = require('gulp-ng-annotate');
 var uglify = require('gulp-uglify');
 
-gulp.task('watch', ['build'], function () {
-    gulp.watch(['static/js/**/*.js', 'static/js/**/**/*.js'], ['build']);
+gulp.task('watch', ['build', 'copy-bower-components'], function () {
+    gulp.watch(['static/js/**/*.js',
+               'static/js/**/**/*.js',
+               ],
+               ['build',
+               'copy-bower-components',
+               ]);
 });
 
 gulp.task('build', function () {
-      return gulp.src('static/js/**/*.js')
+    return gulp.src('static/js/**/*.js')
           .pipe(ngAnnotate())
           .pipe(uglify())
           .pipe(gulp.dest('public/js/'));
+});
+
+gulp.task('copy-bower-components', function() {
+    return gulp.src('static/bower_components/**')
+        .pipe(gulp.dest('public/libs'));
 });
 
