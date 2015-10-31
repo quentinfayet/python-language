b799eed1d631e6774d80cd38adad6c51822ec45c
Quentin <admin@example.com>
2015-10-31 21:01:39 +0100

    Add and configure postgresql python

--- application/language/language/settings.py
+++ application/language/language/settings.py
@@ -77,8 +77,12 @@ WSGI_APPLICATION = 'language.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'language',
+        'USER': 'postgres',
+        'PASSWORD': 'toto',
+        'HOST': 'postgres',
+        'PORT': '5432'
     }
 }
 
