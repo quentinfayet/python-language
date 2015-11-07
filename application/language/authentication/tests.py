807d9d5bf83288b39cf0ec6febf92656f0a843bc
quentinfayet <fayet.quentin@gmail.com>
2015-11-07 23:43:19 +0100

    Write test for Account

--- application/language/authentication/tests.py
+++ application/language/authentication/tests.py
@@ -1,3 +1,30 @@
 from django.test import TestCase
+from authentication.models import Account
 
-# Create your tests here.
+
+class AccountTest(TestCase):
+    def test_create_account(self,
+                            email="test@email.com",
+                            password="password",
+                            username="username"):
+        account = Account.objects.create_user(email=email, password=password,
+                                              username=username)
+        self.assertTrue(isinstance(account, Account))
+
+    def test_create_account_without_email_raises_valueError(self,
+                                                            email="",
+                                                            password="password",
+                                                            username="username"):
+        self.assertRaises(ValueError,
+                          Account.objects.create_user,
+                          email=email,
+                          password=password,
+                          username=username)
+
+    def test_create_account_without_username_raises_valueError(self,
+                                                               email="email",
+                                                               password="pass"):
+        self.assertRaises(ValueError,
+                          Account.objects.create_user,
+                          email=email,
+                          password=password)
---
+++ application/language/language/test_settings.py
@@ -0,0 +1,10 @@
+DATABASES = {
+        'default': {
+                    'ENGINE': 'django.db.backends.sqlite3',
+                    'NAME': 'my_db',
+                    'USER': '',
+                    'PASSWORD': '',
+                    'HOST': '',
+                    'PORT': '',
+        }
+}
