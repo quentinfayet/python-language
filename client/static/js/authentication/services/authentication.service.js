f82a0ad8cdfe15267eb2d1b48c312b339280d701
Quentin Fayet <quentin.fayet@reputationvip.com>
2015-11-13 17:41:26 +0100

    Complete angular auth service

--- client/static/js/authentication/services/authentication.service.js
+++ client/static/js/authentication/services/authentication.service.js
@@ -4,10 +4,51 @@ angular
 
 function Authentication($cookies, $http) {
     var authentication = {
-        register: register
+        register: register,
+        login: login,
+        getAuthenticatedAccount: getAuthenticatedAccount,
+        isAuthenticated: isAuthenticated,
+        setAuthenticatedAccount: setAuthenticatedAccount,
+        unauthenticate: unauthenticate
     };
 
     return authentication;
+
+    function unauthenticate() {
+        delete $cookies.authenticatedAccount;
+    }
+
+    function setAuthenticatedAccount(account) {
+        $cookies.authenticatedAccount = JSON.stringify(account);
+    }
+
+    function isAuthenticated() {
+        return !!$cookies.authenticatedAccount;
+    }
+
+    function getAuthenticatedAccount() {
+        if (!$cookies.authenticatedAccount) {
+            return;
+        }
+
+        return JSON.parse($cookies.authenticatedAccount);
+    }
+
+    function login(email, password) {
+        return $http.post('/api/v1/auth/login/', {
+            email: email,
+            password: password
+        }).then(loginSuccess, loginError);
+
+        function loginSuccess(data, status, headers, config) {
+            Authentication.setAuthenticatedAccount(data.data);
+            windows.location = '/';
+        }
+
+        function loginError(data, status, headers, config) {
+            console.error('Failed to authenticate');
+        }
+    }
 
     function register(email, password, username) {
         return $http.post('/api/v1/accounts/', {
@@ -17,3 +58,4 @@ function Authentication($cookies, $http) {
         });
     }
 }
+
